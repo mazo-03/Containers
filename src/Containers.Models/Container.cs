bf2d9da8a4304d33d2a7ccd5a633194587830dfe
s31162 <s31162@pjwstk.edu.pl>
2025-04-18 16:21:15 +0200

    models done

--- src/Containers.Models/Container.cs
+++ src/Containers.Models/Container.cs
@@ -2,5 +2,8 @@
 
 public class Container
 {
-    
+    public int ID { get; set; }
+    public int ContainerTypeId { get; set; }
+    public bool IsHazardious{get;set;}
+    public String Name { get; set; }
 }
